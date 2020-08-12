685270f707257f9546c4f44d73b664eae603d1bc
Denny Alfath <dennyalfath812@gmail.com>
2020-08-12 12:14:23 +0700

    Initial Commit

---
+++ NewVolgoApp/Model/GoalModel.swift
@@ -0,0 +1,9 @@
+//
+//  GoalModel.swift
+//  NewVolgoApp
+//
+//  Created by Denny Alfath on 12/08/20.
+//  Copyright © 2020 Denny Alfath. All rights reserved.
+//
+
+import Foundation
---
+++ NewVolgoApp/Model/JourneyModel.swift
@@ -0,0 +1,9 @@
+//
+//  JourneyModel.swift
+//  NewVolgoApp
+//
+//  Created by Denny Alfath on 12/08/20.
+//  Copyright © 2020 Denny Alfath. All rights reserved.
+//
+
+import Foundation
