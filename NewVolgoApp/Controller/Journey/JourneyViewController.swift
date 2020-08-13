44ec80075e4445834f47f164d54dd2876030e995
Jogi Oktavianus <34507947+JogiOktavianus@users.noreply.github.com>
2020-08-14 02:38:10 +0700

    adding a new JourneyViewControllerCell Adding a new UILabel Adding TextView

--- NewVolgoApp/Controller/Journey/JourneyViewController.swift
+++ NewVolgoApp/Controller/Journey/JourneyViewController.swift
@@ -10,9 +10,35 @@ import UIKit
 
 class JourneyViewController: UIViewController{
 
+    var image = [UIImage(named: "img-child")]
+    @IBOutlet weak var collectionView: UICollectionView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        collectionView.delegate = self
+        collectionView.dataSource = self
     }
 
 }
 
+extension JourneyViewController: UICollectionViewDelegate{
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        collectionView.deselectItem(at: indexPath, animated: true)
+        print("You Tapped Me")
+    }
+}
+
+extension JourneyViewController: UICollectionViewDataSource{
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        2
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath) as! JourneyCollectionViewCell
+        return cell
+    }
+}
+
+//extension JourneyViewController: UICollectionViewDelegateFlowLayout{
+//
+//}
