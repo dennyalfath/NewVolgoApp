d9014350b75102d69c56d1ad05f676638fc7a249
Jogi Oktavianus <34507947+JogiOktavianus@users.noreply.github.com>
2020-08-16 02:13:03 +0700

    Adding a Placeholder for UITextView

--- NewVolgoApp/Controller/Journey/AddJourneyViewController.swift
+++ NewVolgoApp/Controller/Journey/AddJourneyViewController.swift
@@ -8,24 +8,68 @@
 
 import UIKit
 
-class AddJourneyViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+class AddJourneyViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate, UITextViewDelegate{
 
     @IBOutlet weak var imgView: UIImageView!
+    @IBOutlet weak var textField: UITextField!
+    @IBOutlet weak var textView: UITextView!
+    @IBOutlet weak var dateLabel: UILabel!
+    @IBOutlet weak var datePicker: UIDatePicker!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        textView.text = "Your journey description"
+        textView.textColor = UIColor.lightGray
+        textView.returnKeyType = .done
+        textView.delegate = self
+        self.textView.delegate = self
+        
+        //Adding bottom line for Textfield Title
+        let bottomLine = CALayer()
+        bottomLine.frame = CGRect(x: 0, y: textField.frame.height - 2, width: textField.frame.width, height: 2)
+        bottomLine.backgroundColor = UIColor.init(red: 83/255, green: 87/255, blue: 94/255, alpha: 1).cgColor
+    
+        textField.layer.addSublayer(bottomLine)
+        
+        
+    
+        NotificationCenter.default.addObserver(self, selector: #selector(AddJourneyViewController.updateTextView(notification:)), name: UIResponder.keyboardWillChangeFrameNotification, object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(AddJourneyViewController.updateTextView(notification:)), name: UIResponder.keyboardWillHideNotification , object: nil)
     }
     
+    //MARK: - UITextView Delegate
+    func textViewDidBeginEditing(_ textView: UITextView) {
+        if textView.text == "Your journey description"{
+            textView.text = ""
+            textView.textColor = UIColor.black
+        }
+    }
+    
+    func textView(_ textView: UITextView, shouldChangeTextIn range: NSRange, replacementText text: String) -> Bool {
+        if text == "\n"{
+            textView.resignFirstResponder()
+        }
+        return true
+    }
+    
+    //MARK: - Keyboard function Show and Hide
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        super.touchesBegan(touches, with: event)
+        //self.view.endEditing(true)
+        self.textView.resignFirstResponder()
+    }
+    
+    //MARK: - ActionSheet
     @IBAction func addButtonImg(_ sender: Any) {
+        
         
         let imagePickerController = UIImagePickerController()
         imagePickerController.delegate = self
         
-        
+        //ActionSheet Title
         let actionSheet = UIAlertController(title: "Photo Source", message: "Choose a source", preferredStyle: .actionSheet)
         
+        //Access to the camera
         actionSheet.addAction(UIAlertAction(title: "Camera", style: .default, handler: {(action: UIAlertAction) in }))
         if UIImagePickerController.isSourceTypeAvailable(.camera){
             imagePickerController.sourceType = .camera
@@ -41,10 +85,9 @@ class AddJourneyViewController: UIViewController, UIImagePickerControllerDelegat
         self.present(actionSheet, animated: true, completion: nil)
         
     }
-    
+    //Picking image from library
     func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
         let image = info [UIImagePickerController.InfoKey.originalImage] as! UIImage
-        
         imgView.image = image
         picker.dismiss(animated: true, completion: nil)
     }
@@ -53,5 +96,19 @@ class AddJourneyViewController: UIViewController, UIImagePickerControllerDelegat
         picker.dismiss(animated: true, completion: nil)
     }
     
-
+    //Function textView can be scrolling and Keyboard Setting
+    @objc func updateTextView (notification: Notification){
+        let userInfo = notification.userInfo!
+        
+        let keyboardEndFrameScreenCoordinates = (userInfo[UIResponder.keyboardFrameEndUserInfoKey] as! NSValue).cgRectValue
+        let keyboardEndFrame = self.view.convert(keyboardEndFrameScreenCoordinates, to: view.window)
+        
+        if notification.name == UIResponder.keyboardWillShowNotification{
+            textView.contentInset = UIEdgeInsets.zero
+        } else {
+            textView.contentInset = UIEdgeInsets(top: 0, left: 0, bottom: keyboardEndFrameScreenCoordinates.height, right: 0)
+            textView.scrollIndicatorInsets = textView.contentInset
+        }
+        textView.scrollRangeToVisible(textView.selectedRange)
+    }
 }
